918910a3e2e4f746b2539c2b56088b081f9bc829
inwoo <hodolee246@naver.com>
2021-04-07 23:36:02 +0900

    양방향 매핑으로 변경

--- jpa-sample01/src/main/java/com/example/demo/domain/Team.java
+++ jpa-sample01/src/main/java/com/example/demo/domain/Team.java
@@ -4,6 +4,8 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Setter
@@ -13,6 +15,8 @@ public class Team {
     @Id @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    @OneToMany(mappedBy = "team")
     @Column(name = "TEAM_NAME")
-    private String name;
+    private List<Member> members = new ArrayList<>();
+
 }
